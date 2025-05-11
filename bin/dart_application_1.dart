31b457f761e28c33498b7fc013236399c174c6f7
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-05-11 15:21:11 -0700

    Tipos de Variables

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -18,4 +18,43 @@ void main(List<String> arguments) {
   alumno = "Marcos";
   print(alumno);
 
+  // Tipos de Variables
+  int edad2 = 30; // Declarando una variable de tipo INT para que sepa que solo es int
+  double altura2 = 31.1;  // Declarando una variable de tipo DOUBLE
+
+  // La palabra reservada num puede almacenar tanto valores enteros como decimales
+  num age3 = 31;
+  num altura3 = 32.1; 
+  
+
+  // Variables de cadenas de texto - Strings
+String apellido = 'Franco';
+apellido = "Duarte";
+apellido = "Ovelar";
+
+// Concatenación 
+String concatena = alumno + apellido;
+print(concatena);
+
+String concatena2 = "Nombre: $alumno , Apellido: $apellido, Edad: $edad, Altura: $altura ";
+print(concatena2);
+
+// Variables Booleanas
+bool soyFeliz = true;
+bool estoyTriste = false;
+
+// Variables Tipo Dinámico
+
+dynamic ejemplo = "Esto es meramente un ejemplo";
+print(ejemplo);
+ejemplo = 90;
+print(ejemplo);
+
+// Tipos Fijos 
+final String ejemplo2 = "Hello";
+print(ejemplo2);
+
+const String ejemplo3 = "My friend";
+print(ejemplo3);
+
 }
